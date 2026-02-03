6632ddae9714f563eeab8b993f12029f9c99fa53
xpabl <xpabl@192.168.1.218>
2026-02-03 11:58:05 +0100

    Día 7: Ajustes de estilos y diseño de Contactos

--- contactos.php
+++ contactos.php
@@ -1,12 +1,9 @@
 <?php include 'includes/header.php'; ?>
-
 <main class="contenedor centrar-todo">
     <h1 class="titulo-gigante color-azul">Contacto</h1>
     <p class="texto-destacado">Ponte en contacto con Lavandi Express Alcobendas.</p>
-    
     <div class="alerta-informativa">
-        Estamos diseñando el formulario para que puedas escribirnos pronto.
+        🚀 Estamos diseñando el nuevo formulario. ¡Escríbenos pronto!
     </div>
 </main>
-
 <?php include 'includes/footer.php'; ?>
